9d54323777dee20a9ae4bf5b0ef51292f87bce78
Matt Krueger <mkrueger@rstms.net>
2024-08-09 02:47:58 -0600

    remove commented code, bugfix enter failed confirm

--- util/confirm.go
+++ util/confirm.go
@@ -3,17 +3,16 @@ package util
 import (
     "fmt"
     "os"
-    //"github.com/eiannone/keyboard"
     "golang.org/x/term"
 )
 
 const DELAY = 100
 
-func getKey() (rune, error) {
+func GetKey() (rune, error) {
     var ret rune
     oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
     if err != nil {
-        return ret, fmt.Errorf("Error setting stdin to raw mode: %v\n", err)
+        return ret, fmt.Errorf("Failed setting stdin to raw mode: %v\n", err)
     }
     defer term.Restore(int(os.Stdin.Fd()), oldState)
     buf := make([]byte, 1)
@@ -27,34 +26,14 @@ func getKey() (rune, error) {
 
 func Confirm(prompt string) (bool, error) {
     fmt.Printf("%s? [Y/n] ", prompt)
-    /*
-    err := keyboard.Open()
-    if err != nil {
-	return false, err
-    }
-
-    defer func() {
-        fmt.Printf("closing keyboard...")
-        _ = keyboard.Close()
-        fmt.Printf("keboard closed")
-    }()
-    
-    for {
-        fmt.Println("GetKey...")
-	key, _, err := keyboard.GetKey()
-	if err != nil {
-            return false, err
-        }
-        fmt.Printf("GetKey returned key=%v err=%v\n", key, err)
-    */
-    key, err := getKey()
+    key, err := GetKey()
     if err != nil {
         return false, err
     }
 
     ret := false
     switch key {
-        case 'y','Y', 0:
+    case 'y','Y', '\r', '\n':
 	    fmt.Println("yes")
             ret = true
 	default:
